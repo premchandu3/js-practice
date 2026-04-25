a99181a79f7dad7d5e8eca77ee77a63b9a5c7119
unknown <premchandu372@gmail.com>
2026-04-25 14:30:19 +0530

    Added anagram

---
+++ anagram.js
@@ -0,0 +1,57 @@
+// function anagram(word1,word2){
+// we cant use sort for string for that we use split & join 
+// ---> it turns string into an array
+//     let res= word1.split("").sort().join("");
+//     let ser= word2.split("").sort().join("");
+//     if(res == ser){
+//         console.log("Given words are anagram");
+//     }
+//     else{
+//         console.log("Given words are not anagram");
+//     }
+//     return res,ser ;
+// };
+
+// anagram("listend", "silent");
+
+// ---- > anagram wiyhout using sort method
+// ** First We need to check the lenth of two strings
+function anagram(word1, word2) {
+    if (word1.length !== word2.length) {
+        console.log("Given words are not anagram")
+        return;
+    };
+
+// ** we have to count the elements in the strings
+    let count = {};
+
+    for (let i = 0; i <= word1.length - 1; i++) {
+        let char = word1[i];
+        if (count[char]) {
+            count[char]++
+        }
+        else {
+            count[char] = 1;
+        }
+    }
+    for (let i = 0; i <= word2.length - 1; i++) {
+        let char = word2[i];
+        if (count[char]) {
+            count[char]--
+        }
+        else {
+            console.log("Given words are not anagram");
+            return;
+        }
+    }
+
+    for (let keys in count) {
+        if (count[keys] !== 0) {
+            console.log("Given words are not anagram");
+            return;
+        }
+    }
+    console.log("Given Words are anagram");
+}
+
+anagram("listend", "silent");
--- even_or_odd.js
+++ even_or_odd.js
@@ -1,8 +1,17 @@
-let number = 5;
+// let number = 5;
 
-if (number % 2 === 0) {
-    console.log("Number is even");
-}
-else {
-    console.log("Number is odd");
-}
+// if (number % 2 === 0) {
+//     console.log("Number is even");
+// }
+// else {
+//     console.log("Number is odd");
+// }
+
+
+wors = "listen"
+for(let i=0;i<=wors.length-1;i++){
+    console.log(i);
+    return;
+    console.log(wors[i]);
+};
+
